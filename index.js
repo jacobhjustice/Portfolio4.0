ef99d1e41c3f2de462153b4c77cd79ee9161e6b1
jacobhjustice <jacobhj1997@gmail.com>
2018-10-19 14:42:06 -0500

    Initial portfolio commit with about and work experience

---
+++ index.js
@@ -0,0 +1,20 @@
+function About(){
+    document.getElementById("fillable").innerHTML = document.getElementById("About").innerHTML;
+}
+
+function Experience(){
+    document.getElementById("fillable").innerHTML = document.getElementById("Experience").innerHTML;
+}
+
+function Projects(){
+    document.getElementById("fillable").innerHTML = document.getElementById("Projects").innerHTML;
+}
+
+function Personal(){
+    document.getElementById("fillable").innerHTML = document.getElementById("Personal").innerHTML;
+}
+
+function Contact(){
+    document.getElementById("fillable").innerHTML = document.getElementById("Contact").innerHTML;
+}
+
