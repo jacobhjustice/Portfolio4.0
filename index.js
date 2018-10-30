5aa2bc3bd9a8f275bb587b38b0ac25401bcb5711
jacobhjustice <jacobhj1997@gmail.com>
2018-10-30 13:55:19 -0500

    updates to personal / experience

--- index.js
+++ index.js
@@ -12,17 +12,29 @@ function Projects(){
 
 function Personal(){
     document.getElementById("fillable").innerHTML = document.getElementById("Personal").innerHTML;
+    Studies.buildStudyTableHTML();
 }
 
 function Contact(){
     document.getElementById("fillable").innerHTML = document.getElementById("Contact").innerHTML;
 }
 
-function onClickJob(elem){
+function onClickHobby(elem) {
+    var extra = elem.nextElementSibling;
+    var isHidden = elem.dataset.hidden;
+    if(isHidden == "true") {
+        extra.style.display = "table-row"
+    } else {
+        extra.style.display = "none";
+    }
+    elem.dataset.hidden = isHidden != "true"
+}
+
+function onClickJob(elem) {
     var id = elem.id;
     if(id == "UDA") {
         togglePopup(`
-        I worked three co-op terms at UDA, with my primary job being to maintain and enhance the ConstructionOnline web app by working closely with the product manager to implement any features to fix bugs or improve the over all product. Some major projects that came from this include the following.
+        I worked three co-op terms at UDA, with my primary job being to maintain and enhance the ConstructionOnline web app. The product manager and I collaborated in order to implement various features to improve the over-all polish of the system, by fixing bugs, improving accessibility, and implementing new features. Some major projects that came from this include the following.
         <ul>
         <li>
             Implementing the BCrypt Library into existing backend services in order to move from storing plain-text passwords in the database to a hashed password system
@@ -47,7 +59,13 @@ function onClickJob(elem){
         </li>
         </ul>
         </br</br>
-        Technologies used: Javascript, HTML, CSS, JQuery, .NET, VB, SQL`)
+        Technologies used: Javascript, HTML, CSS, JQuery, .NET, VB, SQL, Github`)
+    } else if(id == 'Workiva') {
+        togglePopup(`
+        I worked this past summer at Workiva on their data sharing team for the web app's document platform. Workiva operates under the agile methodology, which led to me being a core member of the team (despite being an intern), helping in reaching quarterly goals through project planning, QA, code reviews, debugging, documentation, and of course, development. 
+        </br></br>
+        Technologies used: Git, Dart, Go, Redux, Command Line
+        `, '160px');
     }
 }
 
@@ -58,9 +76,26 @@ Popup = {
         PENDING: 2
     },
     currentStatus: 0
+};
+
+Studies = {
+    studiesArray: ["Colossians", "Daniel", "Faith, Hope, & Love", "The Gospels", "Haggai", "Hebrews", "Hosea", "Job", "Jonah", "Prayer", "The Letters of John", "Titus"],
+    buildStudyTableHTML: function() {
+        var row = document.getElementById("BibleCell");
+        for(var i = 0; i < this.studiesArray.length; i++) {
+            var html = `<a href="studies/` + this.studiesArray[i] +`.pdf"  class="bible">
+                        <div class="bookTitle">` + this.studiesArray[i] + `</div>
+                        <div class="page1"></div>
+                    </a>`;
+            var div = document.createElement("div");
+            div.classList.add("bibleWrapper");
+            div.innerHTML = html;
+            row.appendChild(div);
+        }
+    }
 }
 
-function togglePopup(innerHTML) {
+function togglePopup(innerHTML, heightCSS) {
     var elem = document.getElementById("popup");
     if(Popup.currentStatus == Popup.Status.PENDING) {
         return; 
@@ -105,7 +140,7 @@ function togglePopup(innerHTML) {
             // scroll down
             elem.style.top = "20%";
             setTimeout(function(){
-                elem.style.height = "400px";
+                elem.style.height = heightCSS ? heightCSS : "400px";
                 elem.style.width = "700px";
                 setTimeout(function(){
                     wrapper.innerHTML = innerHTML;
